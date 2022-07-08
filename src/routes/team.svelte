84fcc7a67cac1871e1137108e34828f7019449f7
alombi <alombi@hotmail.com>
2022-07-08 14:48:42 +0200

    Improves team page

--- src/routes/team.svelte
+++ src/routes/team.svelte
@@ -1,4 +1,8 @@
 <h1>Shareable team</h1>
-<p>alombi</p>
-<p>mvan231</p>
-<p>FifiTheBulldog</p>
+<p>The team behind Shareable is formed by <a target="blank" href="https://github.com/alombi">alombi</a> (developer), <a href="https://github.com/mvan231">mvan231</a> and <a href="https://github.com/FifiTheBulldog">FifiTheBulldog</a>.</p>
+ 
+<style>
+   a{
+      color:#a1c4fd
+   }
+</style>
