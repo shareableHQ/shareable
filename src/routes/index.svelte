abf045d071cdcb124d1e0cefb3a1d54b6dcc2e34
alombi <alombi@hotmail.com>
2022-07-08 18:56:22 +0200

    Adds favicons

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,7 @@
       let {data, error} = await supabase.from('scripts').select('*')
       data.sort((a, b)=>{
             return a.id - b.id
-         })         
+      })
       data = data.reverse()
       let most_downloaded = await supabase.from('scripts').select('*').order('downloads', { ascending: false })
       most_downloaded = most_downloaded.data.slice(0, 6);
