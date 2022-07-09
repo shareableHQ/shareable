07078ed5d2605092f89f5f2acb0e789a738ee5be
alombi <alombi@hotmail.com>
2022-07-09 19:17:04 +0200

    Improves links in team page

--- src/routes/team.svelte
+++ src/routes/team.svelte
@@ -1,5 +1,5 @@
 <h1>Shareable team</h1>
-<p>The team behind Shareable is formed by <a target="blank" href="https://github.com/alombi">alombi</a> (developer), <a href="https://github.com/mvan231">mvan231</a> and <a href="https://github.com/FifiTheBulldog">FifiTheBulldog</a>.</p>
+<p>The team behind Shareable is formed by <a target="blank" href="https://github.com/alombi">alombi</a> (developer), <a target="blank" href="https://github.com/mvan231">mvan231</a> and <a target="blank" href="https://github.com/FifiTheBulldog">FifiTheBulldog</a>.</p>
  
 <style>
    a{
