bbbd2353a423ea4758a0eeb506f18c46a71791f6
alombi <alombi@hotmail.com>
2022-07-14 22:18:36 +0200

    Adds support for user with more than 200 repositories

--- src/lib/functions/userActions.js
+++ src/lib/functions/userActions.js
@@ -2,8 +2,20 @@ import supabase from '$lib/db';
 
 
 export async function getUserRepo(username) {
+   let req1 = await fetch(`https://api.github.com/users/${username}`)
+   let json1 = await req1.json()
    let req = await fetch(`https://api.github.com/users/${username}/repos?per_page=100&sort=updated`)
    let json = await req.json()
+   if (json1.public_repos > 100) {
+      let req2 = await fetch(`https://api.github.com/users/${username}/repos?per_page=100&sort=updated&page=2`)
+      let json2 = await req2.json()
+      json = json.concat(json2)
+   }
+   if (json1.public_repos > 200) {
+      let req3 = await fetch(`https://api.github.com/users/${username}/repos?per_page=100&sort=updated&page=3`)
+      let json3 = await req3.json()
+      json = json.concat(json3)
+   }
    return json
 }
 
