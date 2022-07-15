cccf6e6c0d39868c276571d32ffc9a6997e619db
alombi <alombi@hotmail.com>
2022-07-15 19:56:42 +0200

    Fixes a problem with some images

--- src/routes/script/[id].svelte
+++ src/routes/script/[id].svelte
@@ -54,7 +54,7 @@
 
    // Fixing images
    onMount(()=>{
-      let branch = script.download_url.replace(`https://raw.githubusercontent.com/${script.author_name}/${script.repo.repo_name}`, '').replace(`/${script.repo.file_path}`, '').replace('/', '')
+      let branch = script.download_url.replace(`https://raw.githubusercontent.com/${script.author_name}/${script.repo.repo_name}`, '').replace(`/${script.repo.file_path.replace(' ', '%20')}`, '').replace('/', '')
       for (let index = 0; index < document.querySelectorAll('img').length; index++) {
          document.querySelectorAll('img')[index].src = document.querySelectorAll('img')[index].src.replace('http://localhost:3000/script/', `https://raw.githubusercontent.com/${script.author_name}/${script.repo.repo_name}/${branch}/`)
          document.querySelectorAll('img')[index].src = document.querySelectorAll('img')[index].src.replace('http://localhost:3000/', `https://raw.githubusercontent.com/${script.author_name}/${script.repo.repo_name}/${branch}/`)
