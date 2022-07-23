74844784b50371fdefabc836c3a70ee3ce0ae03e
alombi <alombi@hotmail.com>
2022-07-23 20:07:14 +0200

    Implements experimental RSS feed

---
+++ src/routes/rss.js
@@ -0,0 +1,36 @@
+import RSS from 'rss';
+import supabase from '$lib/db';
+
+export async function get() {
+   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+   // Get posts
+   let {data, error} = await supabase.from('scripts').select('*')
+   data.sort((a, b)=>{
+         return a.id - b.id
+   })
+   data = data.reverse()
+
+   let feed = new RSS({
+      title: 'Shareable feed',
+      feed_url: 'https://shareable.vercel.app/feed.xml',
+      site_url: 'https://shareable.vercel.app',
+      image_url: 'https://shareable.vercel.app/logo.png'
+   })
+   data.forEach(script => {
+      feed.item({
+         title: `Shareable - New ${script.type.toLowerCase()} published!`,
+         description: `${script.author_name} just published their new <b>${script.type.toLowerCase()}</b>: <a href="https://shareable.vercel.app/script/${script.id}">${script.name}</a>`,
+         url: `https://shareable.vercel.app/script/${script.id}`,
+         date: `${new Date(script.created_at).getDate()} ${months[new Date(script.created_at).getMonth()]} ${new Date(script.created_at).getFullYear()}`
+      })
+   });
+
+   var xml = feed.xml();
+   return {
+      status: 200,
+      body: xml,
+      headers: {
+        'Content-Type': 'application/xml'
+      }
+   }  
+}
