0c3dfc5b44fa56fc9aaa49ff2286b1013470ccbd
alombi <alombi@hotmail.com>
2022-10-07 21:36:00 +0200

    Improves UI

--- src/lib/components/ScriptBox.svelte
+++ src/lib/components/ScriptBox.svelte
@@ -8,7 +8,7 @@
    <div class="script-left">
       <p class="script-title">{script.name}</p>
       <p class="script-author-date">{script.desc}</p>
-      <p class="script-details"><span class="icon tag"><Download /> {script.downloads}</span> <span class="icon tag">{#if script.type == 'Lock Screen Widget'}<Smartphone />{:else if script.type == 'Home Screen Widget'}<LayoutGrid />{:else}<Code />{/if} {script.type}</span></p>
+      <p class="script-details"><span class="icon tag scriptbox-icon"><Download /> {script.downloads}</span> <span class="icon tag scriptbox-icon">{#if script.type == 'Lock Screen Widget'}<Smartphone />{:else if script.type == 'Home Screen Widget'}<LayoutGrid />{:else}<Code />{/if} {script.type}</span></p>
    </div>
    <div class="script-right">
       <a class="script-link get_button" href={"/script/" + script.id}>GET</a>
